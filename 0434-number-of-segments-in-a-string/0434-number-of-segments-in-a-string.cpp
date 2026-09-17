3abd86c7732f526ebf59c5871dedb3f5c6b13cc4
Aarushi Yadav <aarushi.25b01010854@abes.ac.in>
2026-09-17 14:35:10 +0530

    Time: 0 ms (100%), Space: 8.4 MB (20.38%) - LeetHub

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countSegments(string s) {
+        int n=s.length();
+        int segment=0;
+        for(int i=0; i<n;i++){
+            if(s[i]!=' '&&(i==0||s[i-1]==' ')){
+            segment++;
+            }
+        }
+        return segment;
+    }
+};
